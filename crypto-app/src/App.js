6aebc0ea73dc0d62214da53255154a6c1f4d13c9
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-12 15:56:42 +0530

    added pages section

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
 
 function App() {
+
+  
   return (
-    <div className="app">hello</div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} exact/>
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
