6adf272e9a808d1dbfc4320267e904c319472f6d
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-20 00:11:40 +0530

    added tailwind

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,22 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import './App.css';
-import Header from "./components/Header";
-import Homepage from "./Pages/Homepage";
-import CoinPage from "./Pages/CoinPage";
 
 function App() {
-
-  
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Homepage />} exact/>
-          <Route path="/coins/:id" element={<CoinPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+ return (
+   <div>
+    
+   </div>
   );
 }
 
