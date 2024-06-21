ed868a3e44ee0e8087cdf8910c3a9b8d4c81ef4d
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-21 09:33:28 +0530

    added logo

---
+++ crypto-app/src/pages/Crypto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Crypto = () => {
+  return (
+    <div>
+      trendi   </div>
+  )
+}
+
+export default Crypto
---
+++ crypto-app/src/pages/Saved.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Saved = () => {
+  return (
+    <div>
+      s
+    </div>
+  )
+}
+
+export default Saved
