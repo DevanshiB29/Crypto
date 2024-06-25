65e5035b24b2ad869bfd4a0aa1ae78be8227354c
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-06-25 12:06:06 +0530

    added components

--- crypto-app/src/pages/Home.js
+++ crypto-app/src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
 
 const Home = () => {
   return (
-    <main className='bg-red w-full h-full flex flex-col first-letter:
-     content-center items-center relative text-white font-nunito'>
+    <main className=" w-full h-full flex flex-col first-letter:
+     content-center items-center relative text-white font-nunito">
       <div className='w-screen h-screen bg-gray-300 fixed -z-10'/>
     <Logo/>
+    <Navigation/>
 
       <Outlet/>
     </main>
