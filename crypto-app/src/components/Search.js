98400f67714bcb18b53a243cd55116052ae32899
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-07-05 16:14:49 +0530

    filter functionality added

---
+++ crypto-app/src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import searchIcon from "../assets/search-icon.svg";
+const Search = () => {
+  return (
+   <form>
+    <input type="text" name="search"/>
+    <button type="submit">
+        <img src={searchIcon} alt="search"/>
+    </button>
+   </form>
+  )
+}
+
+export default Search
--- crypto-app/src/components/TableComponent.js
+++ crypto-app/src/components/TableComponent.js
@@ -53,12 +53,17 @@ const TableComponent = () => {
                     <span>{data.symbol}</span>
                   </td>
                   <td className="py-4">{data.name}</td>
-                  <td className="py-4">{data.current_price}</td>
+                  <td className="py-4">{
+                    new Intl.NumberFormat("en-IN",{
+                      style:"currency",
+                      currency:"usd"
+                    }).format(data.current_price)
+                    }</td>
                   <td className="py-4">{data.total_volume}</td>
-                  <td className="py-4">{data.market_cap_change_percentage_24h}</td>
-                  <td className="py-4">{Number(data.price_change_percentage_1h_in_currency).toFixed(2)}</td>
-                  <td className="py-4">{Number(data.price_change_percentage_24h_in_currency).toFixed(2)}</td>
-                  <td className="py-4">{Number(data.price_change_percentage_7d_in_currency).toFixed(2)}</td>
+                  <td className="py-4">{data.market_cap_change_percentage_24h}%</td>
+                  <td className={data.price_change_percentage_1h_in_currency>0 ?'text-green py-4':'text-red py-4'}>{Number(data.price_change_percentage_1h_in_currency).toFixed(2)}</td>
+                  <td className={data.price_change_percentage_24h_in_currency>0 ?'text-green py-4':'text-red py-4'}>{Number(data.price_change_percentage_24h_in_currency).toFixed(2)}</td>
+                  <td className={data.price_change_percentage_7d_in_currency>0 ?'text-green py-4':'text-red py-4'}>{Number(data.price_change_percentage_7d_in_currency).toFixed(2)}</td>
                 </tr>
               );
             })}
