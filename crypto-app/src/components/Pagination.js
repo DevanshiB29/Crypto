a3e71f527ab4147978176fe490ecbc5ecc8708d7
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-07-13 22:35:40 +0530

    arrows added

--- crypto-app/src/components/Pagination.js
+++ crypto-app/src/components/Pagination.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import paginationArrow from ""
+import paginationArrow from "../assets/pagination-arrow.svg"
 const Pagination = () => {
   return (
     <div className='flex item-center'>
       <ul>
         <li>
-
+          <img className='w-full h-auto' src={paginationArrow} alt="left"/>
+        </li>
+        <li>...</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>...</li>
+        <li>last page</li>
+        <li>
+          <img className='w-full h-auto' src={paginationArrow} alt="left"/>
         </li>
       </ul>
     </div>
