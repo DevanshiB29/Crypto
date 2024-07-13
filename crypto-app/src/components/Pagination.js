07b0cb1b6132b72015753b443450296e44280fce
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-07-13 22:32:15 +0530

    pagination icon changed

--- crypto-app/src/components/Pagination.js
+++ crypto-app/src/components/Pagination.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import paginationArrow from ""
 const Pagination = () => {
   return (
-    <div>
-      
+    <div className='flex item-center'>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
     </div>
   )
 }
