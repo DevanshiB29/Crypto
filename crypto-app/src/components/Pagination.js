8543ee4cafeba2e363b0d6d3ac0aa6b898869a54
DevanshiB29 <bhusaridevanshi@gmail.com>
2024-07-13 22:40:23 +0530

    styles added to pagination component

--- crypto-app/src/components/Pagination.js
+++ crypto-app/src/components/Pagination.js
@@ -3,14 +3,16 @@ import paginationArrow from "../assets/pagination-arrow.svg"
 const Pagination = () => {
   return (
     <div className='flex item-center'>
-      <ul>
-        <li>
-          <img className='w-full h-auto' src={paginationArrow} alt="left"/>
+      <ul className='flex item-center justify-end text-sm'>
+        <li className='flex items-center'>
+          <button className='outline hover:text-cyan w-8'>
+          <img className='w-full h-auto rotate-180' src={paginationArrow} alt="left"/>
+          </button>
         </li>
-        <li>...</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        <li><button>...</button></li>
+        <li><button>1</button></li>
+        <li><button>2</button></li>
+        <li><button>3</button></li>
         <li>...</li>
         <li>last page</li>
         <li>
